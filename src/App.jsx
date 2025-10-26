619e8a4230e40959c823a61768dc56fddcbd1ec2
Carl Hockey <carl@Carls-MacBook-Air.local>
2025-10-26 19:54:15 +1000

    Replace react-helmet with react-helmet-async to fix UNSAFE_componentWillMount warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { Toaster } from '@/components/ui/toaster';
 import { toast } from '@/components/ui/use-toast';
 import { AppLayout } from '@/components/layout/AppLayout';
